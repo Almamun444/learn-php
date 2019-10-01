d988e97244082c353747aab438f61054ef28971c
Abdullah Al Mamun <mamunhai@gmail.com>
2019-10-01 23:17:43 +0600

    Comparison operator and logical operator

---
+++ comparison.php
@@ -0,0 +1,21 @@
+<?php 
+ ?>
+
+<!doctype html>
+<html >
+<head>
+    <meta charset="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <title>Coomparison</title>
+
+</head>
+
+    <body>
+        <h1><?php echo "Coomparison learing";?></h1>
+        
+           <?php 
+           ?>
+        
+    </body>
+</html>
